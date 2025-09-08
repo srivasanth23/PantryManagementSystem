2d5a6b2dba0b555dfcac39d58550fb5cac0919c4
srivasanth23 <jammulasrivasanth@gmial.com>
2025-09-08 14:59:33 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PantryManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
